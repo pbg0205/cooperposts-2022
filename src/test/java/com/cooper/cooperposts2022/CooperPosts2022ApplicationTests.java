c8c2132a76109796ae8cc484f746507885bf6044
cooper <pbg0205@naver.com>
2022-07-02 15:50:13 +0900

    first commit

---
+++ src/test/java/com/cooper/cooperposts2022/CooperPosts2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.cooperposts2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperPosts2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
