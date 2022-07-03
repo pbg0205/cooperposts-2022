87b244abe4f0db38e8e22fcc80f9abf45101f149
cooper <pbg0205@naver.com>
2022-07-03 16:37:09 +0900

    feat: 게시글 미조회 예외 반환 테스트 코드 추가

---
+++ src/test/java/com/cooper/cooperposts2022/posts/exception/advice/PostExceptionAdviceTest.java
@@ -0,0 +1,51 @@
+package com.cooper.cooperposts2022.posts.exception.advice;
+
+import com.cooper.cooperposts2022.posts.application.PostService;
+import com.cooper.cooperposts2022.posts.exception.PostNotFoundException;
+import com.cooper.cooperposts2022.posts.ui.PostController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(PostController.class)
+@AutoConfigureMockMvc
+class PostExceptionAdviceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    @Test
+    @DisplayName("게시글 조회 실패 반환 메세지를 확인한다")
+    void handlePostNotFoundException() throws Exception {
+        //given
+        String postId = "postId01";
+        given(postService.findById(postId)).willThrow(new PostNotFoundException(postId));
+
+        //when
+        ResultActions result = mockMvc.perform(get("/api/v1/posts/{postId}", postId)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        assertAll(
+                () -> result.andExpect(jsonPath("$.httpStatus").value(HttpStatus.BAD_REQUEST.value())),
+                () -> result.andExpect(jsonPath("$.message")
+                        .value(String.format("게시글을 찾을 수 없습니다 : (postId : %s)", postId)))
+        );
+    }
+
+}
