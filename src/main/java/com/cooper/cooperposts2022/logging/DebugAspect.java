0c9f41215f17fde1bb3279b3583959df2599a3ad
cooper <pbg0205@naver.com>
2022-08-05 23:58:18 +0900

    feat: MDC를 이용한 requestId 추가

--- src/main/java/com/cooper/cooperposts2022/logging/DebugAspect.java
+++ src/main/java/com/cooper/cooperposts2022/logging/DebugAspect.java
@@ -5,8 +5,16 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+import java.util.UUID;
 
 @Slf4j
 @Aspect
@@ -15,6 +23,18 @@ public class DebugAspect {
 
     @Pointcut("@annotation(com.cooper.cooperposts2022.logging.DebugRequired)")
     public void debugRequired() {}
+
+    @Before("debugRequired()")
+    public void logRequest() {
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(
+                RequestContextHolder.getRequestAttributes()))
+                .getRequest();
+
+        String requestId = UUID.randomUUID().toString();
+        MDC.put("requestId", requestId);
+
+        log.debug("[{}] request URL: {}, request method: {}", requestId, request.getRequestURI(), request.getMethod());
+    }
 
     @AfterReturning(pointcut = "debugRequired()")
     public void logParam(JoinPoint joinPoint) {
