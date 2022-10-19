0ddebcea3726f57a56e74768171281686bf7f413
Cooper <pbg0205@naver.com>
2022-10-20 08:37:26 +0900

    refactor: 미사용 import 제거

--- src/main/java/com/cooper/cooperposts2022/common/config/SecurityConfig.java
+++ src/main/java/com/cooper/cooperposts2022/common/config/SecurityConfig.java
@@ -1,15 +1,10 @@
 package com.cooper.cooperposts2022.common.config;
 
 import com.cooper.cooperposts2022.common.provider.UsernamePasswordAuthenticationProvider;
-import com.cooper.cooperposts2022.users.service.JpaUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
--- src/main/java/com/cooper/cooperposts2022/users/dto/UserRegisterRequestDto.java
+++ src/main/java/com/cooper/cooperposts2022/users/dto/UserRegisterRequestDto.java
@@ -11,7 +11,4 @@ public class UserRegisterRequestDto {
     private final String username;
     private final String password;
 
-    public User toEntity() {
-        return new User(username, password);
-    }
 }
