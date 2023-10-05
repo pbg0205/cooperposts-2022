87af2042747fec6b05777a65c746efbe185646ab
cooper <pbg0205@naver.com>
2023-10-06 00:10:35 +0900

    refactor: security config 패키지 이동

--- src/main/java/com/cooper/cooperpost/config/SecurityConfig.java
+++ src/main/java/com/cooper/cooperpost/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.cooper.cooperpost.security.config;
+package com.cooper.cooperpost.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
