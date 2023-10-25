ef1f52eda2422f61c9d867949739beaef3d6afcb
cooper <pbg0205@naver.com>
2023-10-25 14:01:47 +0900

    test: 회원 생성 인수테스트 작성

---
+++ src/test/java/com/cooper/cooperpost/acceptance/base/MemberSteps.java
@@ -0,0 +1,28 @@
+package com.cooper.cooperpost.acceptance.base;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class MemberSteps {
+
+	public static ExtractableResponse<Response> 회원_생성_요청(String email, String password, String name) {
+		Map<String, String> params = new HashMap<>();
+		params.put("email", email);
+		params.put("password", password);
+		params.put("name", "cooper");
+
+		return RestAssured.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(params)
+			.when().post("/api/v1/members")
+			.then().log().all()
+			.statusCode(HttpStatus.OK.value()).extract();
+	}
+}
