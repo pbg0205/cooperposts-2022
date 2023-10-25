b9612d00a3bbb1f72d10603a429546c7bcfecdea
cooper <pbg0205@naver.com>
2023-10-25 14:01:47 +0900

    test: 회원 생성 인수테스트 작성

---
+++ src/test/java/com/cooper/cooperpost/acceptance/base/MemberAcceptanceTest.java
@@ -0,0 +1,24 @@
+package com.cooper.cooperpost.acceptance.base;
+
+import static com.cooper.cooperpost.acceptance.base.MemberSteps.회원_생성_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class MemberAcceptanceTest extends AcceptanceTestBase {
+
+	@DisplayName("회원 가입을 한다")
+	@Test
+	void createMember() {
+		//given, when
+		ExtractableResponse<Response> response = 회원_생성_요청("cooper@gmail.com", "Cooper1234!", "cooper");
+
+		//then
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+	}
+}
